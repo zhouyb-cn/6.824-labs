6a79304587b2196e06f09340b6922fd74f835640
Frans Kaashoek <kaashoek@mit.edu>
2025-02-01 08:29:47 -0500

    update

--- src/kvraft1/server.go
+++ src/kvraft1/server.go
@@ -1,32 +1,28 @@
-package shardgrp
+package kvraft
 
 import (
 	"sync/atomic"
-
 
 	"6.5840/kvraft1/rsm"
 	"6.5840/kvsrv1/rpc"
 	"6.5840/labgob"
 	"6.5840/labrpc"
-	"6.5840/shardkv1/shardgrp/shardrpc"
 	"6.5840/tester1"
+
 )
 
-
 type KVServer struct {
-	gid  tester.Tgid
 	me   int
 	dead int32 // set by Kill()
 	rsm  *rsm.RSM
 
+	// Your definitions here.
 }
-
 
 func (kv *KVServer) DoOp(req any) any {
 	// Your code here
 	return nil
 }
-
 
 func (kv *KVServer) Snapshot() []byte {
 	// Your code here
@@ -37,23 +33,16 @@ func (kv *KVServer) Restore(data []byte) {
 	// Your code here
 }
 
-func (kv *KVServer) Get(args *shardrpc.GetArgs, reply *rpc.GetReply) {
-	// Your code here
+func (kv *KVServer) Get(args *rpc.GetArgs, reply *rpc.GetReply) {
+	// Your code here. Use kv.rsm.Submit() to submit args
+	// You can use go's type casts to turn the any return value
+	// of Submit() into a GetReply: rep.(rpc.GetReply)
 }
 
-func (kv *KVServer) Put(args *shardrpc.PutArgs, reply *rpc.PutReply) {
-	// Your code here
-}
-
-// Freeze the specified shard (i.e., reject future Get/Puts for this
-// shard) and return the key/values stored in that shard.
-func (kv *KVServer) Freeze(args *shardrpc.FreezeArgs, reply *shardrpc.FreezeReply) {
-	// Your code here
-}
-
-// Install the supplied state for the specified shard.
-func (kv *KVServer) InstallShard(args *shardrpc.InstallShardArgs, reply *shardrpc.InstallShardReply) {
-	// Your code here
+func (kv *KVServer) Put(args *rpc.PutArgs, reply *rpc.PutReply) {
+	// Your code here. Use kv.rsm.Submit() to submit args
+	// You can use go's type casts to turn the any return value
+	// of Submit() into a PutReply: rep.(rpc.PutReply)
 }
 
 // the tester calls Kill() when a KVServer instance won't
@@ -79,16 +68,14 @@ func (kv *KVServer) killed() bool {
 func StartKVServer(servers []*labrpc.ClientEnd, gid tester.Tgid, me int, persister *tester.Persister, maxraftstate int) []tester.IService {
 	// call labgob.Register on structures you want
 	// Go's RPC library to marshall/unmarshall.
-	labgob.Register(shardrpc.PutArgs{})
-	labgob.Register(shardrpc.GetArgs{})
-	labgob.Register(shardrpc.FreezeArgs{})
-	labgob.Register(shardrpc.InstallShardArgs{})
-	labgob.Register(shardrpc.DeleteShardArgs{})
 	labgob.Register(rsm.Op{})
+	labgob.Register(rpc.PutArgs{})
+	labgob.Register(rpc.GetArgs{})
 
-	kv := &KVServer{gid: gid, me: me}
+	kv := &KVServer{me: me}
+
+
 	kv.rsm = rsm.MakeRSM(servers, me, persister, maxraftstate, kv)
-
-	// Your code here
+	// You may need initialization code here.
 	return []tester.IService{kv, kv.rsm.Raft()}
 }
