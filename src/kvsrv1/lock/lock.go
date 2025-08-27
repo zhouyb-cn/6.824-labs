94ef2a14b9a51637ae08e327915eb3c7b13eb533
zhouyb <zhouyb.cn@gmail.com>
2025-08-27 08:10:54 +0000

    25-lab-kvsrv1

--- src/kvsrv1/lock/lock.go
+++ src/kvsrv1/lock/lock.go
@@ -1,7 +1,9 @@
 package lock
 
 import (
+	"6.5840/kvsrv1/rpc"
 	"6.5840/kvtest1"
+	"time"
 )
 
 type Lock struct {
@@ -11,6 +13,8 @@ type Lock struct {
 	// MakeLock().
 	ck kvtest.IKVClerk
 	// You may add code here
+	lkey   string
+	lvalue string
 }
 
 // The tester calls MakeLock() and passes in a k/v clerk; your code can
@@ -21,13 +25,32 @@ type Lock struct {
 func MakeLock(ck kvtest.IKVClerk, l string) *Lock {
 	lk := &Lock{ck: ck}
 	// You may add code here
+	lk.lkey = l
+	lk.lvalue = kvtest.RandValue(8)
 	return lk
 }
 
 func (lk *Lock) Acquire() {
 	// Your code here
+	for {
+		value, version, err := lk.ck.Get(lk.lkey)
+		if err == rpc.ErrNoKey || (err == rpc.OK && value == "") {
+			err = lk.ck.Put(lk.lkey, lk.lvalue, version)
+			if err == rpc.OK {
+				return
+			}
+			time.Sleep(10 * time.Millisecond)
+			continue
+		}
+		// 有可能网络问题服务端已经处理过，判断value 直接返回
+		if value == lk.lvalue {
+			return
+		}
+	}
 }
 
 func (lk *Lock) Release() {
 	// Your code here
+	_, version, _ := lk.ck.Get(lk.lkey)
+	lk.ck.Put(lk.lkey, "", version)
 }
